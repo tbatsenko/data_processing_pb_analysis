f7b7abe26fa254c9534189ce0a6e36829fcade39
Tanya Batsenko <tbatsenko@gmail.com>
2019-01-08 11:03:33 +0200

    stage 2

--- my.js
+++
@@ -1,7 +0,0 @@
-
-
-const fileReader = new FileReader();
-const res = fileReader.readAsText('statements.csv');
-
-console.log(res)
-
--- my.py
+++
@@ -1,4 +0,0 @@
-print("Hi, Tanya")
-
-f = open('statements.csv', 'r')
-print(f.readlines())
